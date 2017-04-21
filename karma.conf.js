2802e79c846029c110a5209826aea8a2c53cf73e
Naveenshri <naveenmscinfotech@gmail.com>
2017-04-21 15:19:32 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/cli', 'es6-shim'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
@@ -12,7 +12,7 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma'),
-      require('es6-shim')
+      require('karma-es6-shim')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
